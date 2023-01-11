1e7d1739feb1d04fca17f98e8ac42082c8b7d665
lacey-e <buchiezim@gmail.com>
2023-01-10 23:30:38 -0800

    week2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
+import { TestNameComponent } from './test-name/test-name.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    TestNameComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/test-name/test-name.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { bootstrapApplication } from '@angular/platform-browser';
+
+@Component({
+  selector: 'app-test-name',
+  templateUrl: './test-name.component.html',
+  styleUrls: ['./test-name.component.css']
+})
+export class TestNameComponent {
+
+}
